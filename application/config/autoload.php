31f4c20539e2a364f040c5b7d702ab6e22dbad86
Syihab <syehab94@gmail.com>
2017-12-28 15:01:33 +0700

    Adding currency_helper into autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'menus_helper');
+$autoload['helper'] = array('url', 'menus_helper', 'currency_helper');
 
 /*
 | -------------------------------------------------------------------
